d95ff13b97cfb6620a4a2fda605547cb124bebc8
Logan Donley <ldonley@cloudbees.com>
2021-01-20 16:47:51 -0500

    Changing syntax of output

--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -72,9 +72,9 @@ resource "google_compute_instance" "tomcat-uat" {
 }
 
 output "tomcat_qa_ip" {
-  value = google_compute_instance.tomcat-qa.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.tomcat-qa.network_interface.0.access_config.0.nat_ip
 }
 
 output "tomcat_uat_ip" {
-  value = google_compute_instance.tomcat-uat.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.tomcat-uat.network_interface.0.access_config.0.nat_ip
 }
