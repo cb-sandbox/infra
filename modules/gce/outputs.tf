c6795a7f082db6e3e44e0dcd1ef0abc7e696c249
Logan Donley <ldonley@cloudbees.com>
2021-01-21 17:30:11 -0500

    Removing wildcard and replacing with 0 index

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -1,8 +1,8 @@
 
 output "tomcat_qa_ip" {
-  value = google_compute_instance_from_machine_image.tomcat_qa.*.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance_from_machine_image.tomcat_qa.0.network_interface.0.access_config.0.nat_ip
 }
 
 output "tomcat_uat_ip" {
-  value = google_compute_instance_from_machine_image.tomcat_uat.*.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance_from_machine_image.tomcat_uat.0.network_interface.0.access_config.0.nat_ip
 }
