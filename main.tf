0d1ba162848f37716ea235d23e9a2240a0eb1afc
Kurt Madel <kmadel@mac.com>
2021-09-14 14:24:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
-module "gke" {
-  source = "./modules/gke"
-  cluster_name = var.cluster_name
-  project = var.project
-  region = var.region
-  zone = var.zone
-}
-
 module "gce" {
   source = "./modules/gce"
   cluster_name = var.cluster_name
   project = var.project
   region = var.region
   agent_enabled = var.agent_enabled
+}
+
+  module "gke" {
+  source = "./modules/gke"
+  cluster_name = var.cluster_name
+  project = var.project
+  region = var.region
+  zone = var.zone
+  depends_on = [modules.gce]
 }
