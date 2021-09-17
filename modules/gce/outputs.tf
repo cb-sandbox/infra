2760c365adcc53a4b1e7a680f179c3913deed0fe
Kurt Madel <kmadel@mac.com>
2021-09-17 14:18:09 -0400

    Update outputs.tf

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -6,7 +6,3 @@ output "tomcat_qa_ip" {
 output "tomcat_uat_ip" {
   value = length(google_compute_instance_from_machine_image.tomcat_uat) > 0 ? google_compute_instance_from_machine_image.tomcat_uat.0.network_interface.0.access_config.0.nat_ip : ""
 }
-
-output "network_name" {
-  value = google_compute_network.sandbox_network.name
-}
