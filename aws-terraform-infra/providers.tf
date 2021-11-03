fd8b15680cdde1c552a95260f4a162d9107e7d37
Ian Kurtz <79282169+ikurtz@users.noreply.github.com>
2021-11-03 08:51:28 -0500

    Update providers.tf

--- aws-terraform-infra/providers.tf
+++ aws-terraform-infra/providers.tf
@@ -1,16 +1,13 @@
-terraform {
-  backend "gcs" {
-    bucket = "sandbox_bucket"
-    prefix = "terraform/state"
-  }
-}
+# Kubernetes provider
+# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
 
-provider "google" {
-  project = var.project
-  region = var.region
-}
+# The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
+# You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
 
-provider "google-beta" {
-  project = var.project
-  region  = var.region
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
