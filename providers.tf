dc95f0c0e547733a90c589ab047882f7d6f77fe3
Kurt Madel <kmadel@cloudbees.com>
2021-11-18 09:39:17 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,6 +3,20 @@ terraform {
     bucket = "sandbox_bucket"
     prefix = "terraform/state"
   }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.0.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "~> 4.0.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
 }
 
 provider "google" {
