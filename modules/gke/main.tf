2cc78a5e5a5456baef2c6b61548dcfbe7a0cfd49
Kurt Madel <kmadel@cloudbees.com>
2023-03-07 15:01:42 -0500

    Update main.tf
    Switching to 'sandbox' subnetwork as the `default` is near its limit.

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -12,7 +12,7 @@ resource "google_container_cluster" "primary" {
   location = var.zone
   remove_default_node_pool = true
   initial_node_count = 1
-  subnetwork = "default"
+  subnetwork = "sandbox"
   release_channel {
     channel = "REGULAR"
   }
