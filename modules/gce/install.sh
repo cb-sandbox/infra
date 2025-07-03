5b1217e0749c7360f1d1bdddd7b2104b374409e7
Bob Cauthen <89991256+CBCauthen@users.noreply.github.com>
2025-07-03 09:58:28 -0400

    Updated CloudBeesFlowAgent version

--- modules/gce/install.sh
+++ modules/gce/install.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install openjdk-11-jdk tomcat9 tomcat9-admin mysql-server -y
 
-wget https://downloads.cloudbees.com/cloudbees-cd/preview_release/2020.10.0.144378/linux/CloudBeesFlowAgent-x64-2020.10.0.144378 -O cd-agent-installer
+wget https://downloads.cloudbees.com/cloudbees-cd/Release_2023.10/2023.10.0.169425/linux/CloudBeesFlowAgent-x64-2023.10.0.169425 -O cd-agent-installer
 chmod +x ./cd-agent-installer
 
 ./cd-agent-installer \
